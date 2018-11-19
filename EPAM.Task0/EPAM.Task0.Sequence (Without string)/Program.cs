c0fe6a1db4a4a452f84f4f0ce25843821d8ba4e2
alexander-kudiyarov <45111111+alexander-kudiyarov@users.noreply.github.com>
2018-11-20 01:09:42 +0400

    task 0

---
+++ EPAM.Task0/EPAM.Task0.Sequence (Without string)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace EPAM.Task0.Sequence
+{
+    class Sequence
+    {
+        public void Build(int n)
+        {
+            if (n > 0)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    Console.Write("{0}, ", i);
+                }
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine("N must be greater than 0");
+            }
+        }
+    }
+    class SequenceDemo
+    {
+        static void Main()
+        {
+            Sequence ob = new Sequence();
+            Console.WriteLine("Please, enter value for N (N must be greater than 0)");
+            ob.Build(Int32.Parse(Console.ReadLine()));
+        }
+    }
+}
---
+++ EPAM.Task0/EPAM.Task0.Square (Without string)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EPAM.Task0.Square
+{
+    class Square
+    {
+        public void Draw(int n)
+        {
+            if (n % 2 != 0 & n > 1)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == n / 2 & j == n / 2)
+                        {
+                            Console.Write(' ');
+                        }
+                        else
+                        {
+                            Console.Write('*');
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("N must be odd and greater than one.");
+            }
+        }
+    }
+    class SquareDemo
+    {
+        static void Main()
+        {
+            Square ob = new Square();
+            Console.WriteLine("Please, enter value for N (N must be odd and greater than one)");
+            ob.Draw(Int32.Parse(Console.ReadLine()));
+        }
+    }
+}
