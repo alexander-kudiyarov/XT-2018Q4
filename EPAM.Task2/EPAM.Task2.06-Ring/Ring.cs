be70ffca0723f76f20e3f4f7e43ec95c7ea8bc2d
alexander-kudiyarov <45111111+alexander-kudiyarov@users.noreply.github.com>
2018-12-04 19:51:52 +0400

    ^^

---
+++ EPAM.Task2/EPAM.Task2.06-Ring/Ring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.Task2._06_Ring
+{
+    class Ring
+    {
+    }
+}
