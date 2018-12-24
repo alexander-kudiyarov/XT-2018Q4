51681e65263af6f1002b1dc12d345c8539189e2c
alexander-kudiyarov <45111111+alexander-kudiyarov@users.noreply.github.com>
2018-12-24 22:54:10 +0400

    v 0.99

---
+++ EPAM.Task5/EPAM.Task5.01-BackupSystem/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EPAM.Task5._01_BackupSystem
+{
+    public class Program
+    {
+        private static string sourcePath = @"d:\test";
+        private static string backupPath = sourcePath.Insert(sourcePath.Length, "B");
+        private static string dateFormat = "yyyy-MM-dd HH-mm-ss";
+
+        public static void Main()
+        {
+            Console.WriteLine($"Very Simple Backup System" +
+                $"{Environment.NewLine}" +
+                $"{Environment.NewLine}Select mode:" +
+                $"{Environment.NewLine}Enter <1> for Watch mode" +
+                $"{Environment.NewLine}Enter <2> for Recovery mode" +
+                $"{Environment.NewLine}");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    {
+                        Watch();
+                        Main();
+                        break;
+                    }
+
+                case "2":
+                    {
+                        Recovery();
+                        Main();
+                        break;
+                    }
+
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+        private static void Watch()
+        {
+            DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
+            if (!sourceDirectory.Exists)
+            {
+                Directory.CreateDirectory(sourcePath);
+            }
+
+            foreach (string filePath in Directory.GetFiles(sourcePath, "*.txt", SearchOption.AllDirectories))
+            {
+                FileInfo fi = new FileInfo(filePath);
+                Directory.CreateDirectory(filePath.Replace(sourcePath, backupPath));
+                File.Copy(filePath, filePath.Replace(sourcePath, backupPath).Insert(filePath.Length + 1, $@"\{fi.LastWriteTime.ToString(dateFormat)}"), true);
+            }
+
+            FileSystemWatcher watcher = new FileSystemWatcher(sourcePath, "*.txt");
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            watcher.InternalBufferSize = 65536;
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Renamed += new RenamedEventHandler(OnRename);
+            watcher.Deleted += new FileSystemEventHandler(OnDelete);
+
+            Console.WriteLine(@"Press 'q' to quit the sample.");
+            while (Console.Read() != 'q')
+            {
+            }
+
+            Console.WriteLine("Watching stopped");
+            return;
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            File.Copy(e.FullPath, e.FullPath.Replace(sourcePath, backupPath).Insert(e.FullPath.Length + 1, $@"\{DateTime.Now.ToString(dateFormat)}"), true);
+        }
+
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            Directory.CreateDirectory(e.FullPath.Replace(sourcePath, backupPath));
+            OnChanged(source, e);
+        }
+
+        private static void OnRename(object source, RenamedEventArgs e)
+        {
+            OnCreated(source, e);
+            File.Create(e.OldFullPath.Replace(sourcePath, backupPath).Insert(e.OldFullPath.Length + 1, $@"\{DateTime.Now.ToString(dateFormat)}-deleted"));
+        }
+
+        private static void OnDelete(object source, FileSystemEventArgs e)
+        {
+            File.Create(e.FullPath.Replace(sourcePath, backupPath).Insert(e.FullPath.Length + 1, $@"\{DateTime.Now.ToString(dateFormat)}-deleted"));
+        }
+
+        private static void Recovery()
+        {
+            Console.WriteLine("Enter point of backup");
+            DateTime point = DateTime.Parse(Console.ReadLine());
+            DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
+            if (sourceDirectory.Exists)
+            {
+                Directory.Delete(sourcePath, true);
+            }
+
+            foreach (string path in Directory.GetDirectories(backupPath, "*.txt", SearchOption.AllDirectories))
+            {
+                string[] versions = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+                for (int i = versions.Length - 1; i >= 0; i--)
+                {
+                    if (versions[i].Substring(versions[i].LastIndexOf('-')) == @"-deleted")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        DateTime ver = DateTime.ParseExact(versions[i].Substring(versions[i].LastIndexOf('\\') + 1), dateFormat, null);
+                        if (ver <= point)
+                        {
+                            string filePath = versions[i].Replace(backupPath, sourcePath).Remove(versions[i].LastIndexOf('\\') - 1);
+                            string folderPath = filePath.Remove(filePath.LastIndexOf('\\'));
+                            Directory.CreateDirectory(folderPath);
+                            File.Copy(versions[i], filePath, true);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Backup successful");
+            return;
+        }
+    }
+}
---
+++ EPAM.Task5/EPAM.Task5.01-BackupSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EPAM.Task5.01-BackupSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EPAM.Task5.01-BackupSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cc8c9fd3-8c94-4826-8bb9-f0bc15380883")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
