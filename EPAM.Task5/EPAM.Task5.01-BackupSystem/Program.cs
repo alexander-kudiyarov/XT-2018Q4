fe099f86964fff2be71abd1330976a21823bd6fc
Alexander Kudiyarov <45146218+alexander-kudiyarov@users.noreply.github.com>
2018-12-26 00:55:13 +0400

    Add files via upload

--- EPAM.Task5/EPAM.Task5.01-BackupSystem/Program.cs
+++ EPAM.Task5/EPAM.Task5.01-BackupSystem/Program.cs
@@ -16,19 +16,22 @@ namespace EPAM.Task5._01_BackupSystem
 
         public static void Main()
         {
-            Console.WriteLine("Enter folder path:");
-            sourcePath = Console.ReadLine();
-            if (sourcePath[sourcePath.Length - 1] == '\\')
-            {
-                sourcePath = sourcePath.Remove(sourcePath.Length - 1);
-            }
-            backupPath = sourcePath.Insert(sourcePath.Length, "Backup");
             Console.WriteLine($"Very Simple Backup System" +
                 $"{Environment.NewLine}" +
                 $"{Environment.NewLine}Select mode:" +
                 $"{Environment.NewLine}Enter <1> for Watch mode" +
                 $"{Environment.NewLine}Enter <2> for Recovery mode" +
                 $"{Environment.NewLine}");
+            Console.WriteLine();
+            Console.WriteLine("Enter folder path fot watch or backup");
+            sourcePath = Console.ReadLine();
+            if (sourcePath[sourcePath.Length - 1] == '\\')
+            {
+                sourcePath = sourcePath.Remove(sourcePath.Length - 1);
+            }
+
+            Console.WriteLine(sourcePath);
+            backupPath = sourcePath.Insert(sourcePath.Length, "Backup");
             switch (Console.ReadLine())
             {
                 case "1":
@@ -65,13 +68,12 @@ namespace EPAM.Task5._01_BackupSystem
                 FileInfo fi = new FileInfo(filePath);
                 Directory.CreateDirectory(filePath.Replace(sourcePath, backupPath));
                 File.Copy(filePath, filePath.Insert(filePath.Length, $@"\{fi.LastWriteTime.ToString(dateFormat)}").Replace(sourcePath, backupPath), true);
-                Console.WriteLine(filePath.Insert(filePath.Length, $@"\{fi.LastWriteTime.ToString(dateFormat)}").Replace(sourcePath, backupPath));
             }
 
             FileSystemWatcher watcher = new FileSystemWatcher(sourcePath, "*.txt");
             watcher.IncludeSubdirectories = true;
             watcher.EnableRaisingEvents = true;
-            watcher.InternalBufferSize = 65536;
+            watcher.InternalBufferSize = 655360;
 
             watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
             watcher.Changed += new FileSystemEventHandler(OnChanged);
@@ -90,30 +92,33 @@ namespace EPAM.Task5._01_BackupSystem
 
         private static void OnChanged(object source, FileSystemEventArgs e)
         {
-            File.Copy(e.FullPath, e.FullPath.Replace(sourcePath, backupPath).Insert(e.FullPath.Length + 1, $@"\{DateTime.Now.ToString(dateFormat)}"), true);
+            Console.WriteLine("OnChanged");
+            File.Copy(e.FullPath, e.FullPath.Insert(e.FullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}").Replace(sourcePath, backupPath), true);
         }
 
         private static void OnCreated(object source, FileSystemEventArgs e)
         {
+            Console.WriteLine("OnCreated");
             Directory.CreateDirectory(e.FullPath.Replace(sourcePath, backupPath));
             OnChanged(source, e);
+            Thread.Sleep(3);
         }
 
         private static void OnRename(object source, RenamedEventArgs e)
         {
             OnCreated(source, e);
-            File.Create(e.OldFullPath.Replace(sourcePath, backupPath).Insert(e.OldFullPath.Length + 1, $@"\{DateTime.Now.ToString(dateFormat)}-deleted"));
+            File.Create(e.OldFullPath.Insert(e.OldFullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}-deleted").Replace(sourcePath, backupPath));
         }
 
         private static void OnDelete(object source, FileSystemEventArgs e)
         {
-            File.Create(e.FullPath.Replace(sourcePath, backupPath).Insert(e.FullPath.Length + 1, $@"\{DateTime.Now.ToString(dateFormat)}-deleted"));
+            File.Create(e.FullPath.Insert(e.FullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}-deleted").Replace(sourcePath, backupPath));
         }
 
         private static void Recovery()
         {
             Console.WriteLine("Enter date and time of backup ('dd-MM-yyyy hh:mm:ss' for example)");
-            DateTime point = DateTime.Parse(Console.ReadLine());
+            DateTime point = DateTime.ParseExact(Console.ReadLine(), dateFormat, null);
             DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
             if (sourceDirectory.Exists)
             {
@@ -134,7 +139,7 @@ namespace EPAM.Task5._01_BackupSystem
                         DateTime ver = DateTime.ParseExact(versions[i].Substring(versions[i].LastIndexOf('\\') + 1), dateFormat, null);
                         if (ver <= point)
                         {
-                            string filePath = versions[i].Replace(backupPath, sourcePath).Remove(versions[i].LastIndexOf('\\') - 1);
+                            string filePath = versions[i].Remove(versions[i].LastIndexOf('\\')).Replace(backupPath, sourcePath);
                             string folderPath = filePath.Remove(filePath.LastIndexOf('\\'));
                             Directory.CreateDirectory(folderPath);
                             File.Copy(versions[i], filePath, true);
