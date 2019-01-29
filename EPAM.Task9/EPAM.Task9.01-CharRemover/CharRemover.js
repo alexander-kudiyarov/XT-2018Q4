fe621739d2e3d5c4460149536aa51e5b6b15dda0
Alexander Kudiyarov <45146218+alexander-kudiyarov@users.noreply.github.com>
2019-01-29 21:27:43 +0400

    task9 1/4

---
+++ EPAM.Task9/EPAM.Task9.01-CharRemover/CharRemover.js
@@ -0,0 +1,30 @@
+function CharRemover() {
+	var input = "У попа была cобака и собака и собака и собака";
+	var splitInput;
+	var joinedSplitInput = input;
+    var sepators = [' ', '	', '?', '!', ':', ';', '.'];
+    
+	sepators.forEach(function(element) {
+		splitInput = joinedSplitInput.split(element);
+		joinedSplitInput = splitInput.join();
+    });
+    
+	splitInput = joinedSplitInput.split(',');
+    var charsToRemove = [];
+    
+	splitInput.forEach(function(element) {
+		for(var i = 0; i < element.length - 1; i++) {
+			for(var j = i + 1; j < element.length; j++) {
+				if(element[i] == element[j]) {
+					charsToRemove.push(element[i]);
+				}
+			}
+		}
+    });
+    
+	for(var i = 0; i < input.length; i++) {
+		input = input.replace(charsToRemove[0], '').replace(charsToRemove[1], '');
+    }
+    
+	console.log(input);
+}
