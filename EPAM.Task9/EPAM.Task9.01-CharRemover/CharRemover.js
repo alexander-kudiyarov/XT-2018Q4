e676ef46a7830aa2f57f91a924c0047798019486
Alexander Kudiyarov <45146218+alexander-kudiyarov@users.noreply.github.com>
2019-01-29 23:43:31 +0400

    Update CharRemover.js

--- EPAM.Task9/EPAM.Task9.01-CharRemover/CharRemover.js
+++ EPAM.Task9/EPAM.Task9.01-CharRemover/CharRemover.js
@@ -1,29 +1,29 @@
 function CharRemover(input) {
-	var splitInput;
-	var joinedSplitInput = input;
-	var sepators = [' ', '	', '?', '!', ':', ';', '.'];
+    var splitInput;
+    var joinedSplitInput = input;
+    var sepators = [' ', '    ', '?', '!', ':', ';', '.'];
 
-	sepators.forEach(function(element) {
-		splitInput = joinedSplitInput.split(element);
-		joinedSplitInput = splitInput.join();
-	});
+    sepators.forEach(function(element) {
+        splitInput = joinedSplitInput.split(element);
+        joinedSplitInput = splitInput.join();
+    });
 
-	splitInput = joinedSplitInput.split(',');
-	var charsToRemove = [];
-		
-	splitInput.forEach(function(element) {
-		for(var i = 0; i < element.length - 1; i++) {
-			for(var j = i + 1; j < element.length; j++) {
-				if(element[i] == element[j]) {
-					charsToRemove.push(element[i]);
-				}
-			}
-		}
-	});
+    splitInput = joinedSplitInput.split(',');
+    var charsToRemove = [];
+        
+    splitInput.forEach(function(element) {
+        for(var i = 0; i < element.length - 1; i++) {
+            for(var j = i + 1; j < element.length; j++) {
+                if(element[i] == element[j]) {
+                    charsToRemove.push(element[i]);
+                }
+            }
+        }
+    });
 
-	for(var i = 0; i < input.length; i++) {
-		input = input.replace(charsToRemove[0], '').replace(charsToRemove[1], '');
-	}
+    for(var i = 0; i < input.length; i++) {
+        input = input.replace(charsToRemove[0], '').replace(charsToRemove[1], '');
+    }
 
-	console.log(input);
+    console.log(input);
 }
