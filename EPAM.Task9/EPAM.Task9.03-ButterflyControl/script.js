87b0054320d9761421fedd950e2472a075815bd3
Alexander Kudiyarov <45146218+alexander-kudiyarov@users.noreply.github.com>
2019-01-30 23:22:23 +0400

    task9 3/4

--- EPAM.Task9/EPAM.Task9.03-ButterflyControl/script.js
+++ EPAM.Task9/EPAM.Task9.03-ButterflyControl/script.js
@@ -1,10 +1,9 @@
-function move(from, to) {
+function moveSelected(from, to) {
     var selectOptions = document.getElementById(from);
     for (var i = 0; i < selectOptions.length; i++) {
         var option = selectOptions[i];
         if (option.selected) {
-            document.getElementById(from).removeChild(option);
-            document.getElementById(to).appendChild(option);
+            move(from, to, option);
             i--;
         }
     }
@@ -14,8 +13,12 @@ function moveAll(from, to) {
     var options = document.getElementById(from);
     for (var i = 0; i < options.length; i++) {
         var option = options[i];
-        document.getElementById(from).removeChild(option);
-        document.getElementById(to).appendChild(option);
+        move(from, to, option);
         i--;
     }
+}
+
+function move(from, to, option) {
+    document.getElementById(from).removeChild(option);
+    document.getElementById(to).appendChild(option);
 }
