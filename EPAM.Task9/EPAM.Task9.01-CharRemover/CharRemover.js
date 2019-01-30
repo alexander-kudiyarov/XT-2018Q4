69d07db570214bc10f92181919abfdd24357b5e8
Alexander Kudiyarov <45146218+alexander-kudiyarov@users.noreply.github.com>
2019-01-30 23:33:24 +0400

    fixes

--- EPAM.Task9/EPAM.Task9.01-CharRemover/CharRemover.js
+++ EPAM.Task9/EPAM.Task9.01-CharRemover/CharRemover.js
@@ -12,16 +12,16 @@ function CharRemover(input) {
     var charsToRemove = [];
 
     splitInput.forEach(function (element) {
-        for (var i = 0; i < element.length - 1; i++) {
-            for (var j = i + 1; j < element.length; j++) {
-                if (element[i] == element[j]) {
+        for(var i = 0; i < element.length - 1; i++) {
+            for(var j = i + 1; j < element.length; j++) {
+                if(element[i] == element[j]) {
                     charsToRemove.push(element[i]);
                 }
             }
         }
     });
 
-    for (var i = 0; i < input.length; i++) {
+    for(var i = 0; i < input.length; i++) {
         charsToRemove.forEach(function (element) {
             input = input.replace(element, '');
         });
--- EPAM.Task9/EPAM.Task9.03-ButterflyControl/script.js
+++ EPAM.Task9/EPAM.Task9.03-ButterflyControl/script.js
@@ -1,24 +1,38 @@
 function moveSelected(from, to) {
     var selectOptions = document.getElementById(from);
-    for (var i = 0; i < selectOptions.length; i++) {
+
+    for(var i = 0; i < selectOptions.length; i++) {
         var option = selectOptions[i];
         if (option.selected) {
             move(from, to, option);
             i--;
         }
     }
+
+    clearSelected(to);
 }
 
 function moveAll(from, to) {
     var options = document.getElementById(from);
-    for (var i = 0; i < options.length; i++) {
+
+    for(var i = 0; i < options.length; i++) {
         var option = options[i];
         move(from, to, option);
         i--;
     }
+
+    clearSelected(to);
 }
 
 function move(from, to, option) {
     document.getElementById(from).removeChild(option);
     document.getElementById(to).appendChild(option);
+}
+
+function clearSelected(list) {
+    var elements = document.getElementById(list).options;
+
+    for(var i = 0; i < elements.length; i++){
+        elements[i].selected = false;
+    }
 }
