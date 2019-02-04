c3c8dbf7c2e9cf2e5e38844162f24f6854547524
Alexander Kudiyarov <45146218+alexander-kudiyarov@users.noreply.github.com>
2019-02-04 23:18:51 +0400

    fixes

---
+++ EPAM.Task5/EPAM.Task5.01-BackupSystem/BackupSystem.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace EPAM.Task5._01_BackupSystem
+{
+    public class BackupSystem
+    {
+        private static string dateFormat = "yyyy-MM-dd HH-mm-ss";
+
+        public static string BackupPath { get; set; }
+
+        public static string SourcePath { get; set; }
+
+        public static void Recovery()
+        {
+            Console.WriteLine($"Enter date and time of backup ('dd-MM-yyyy hh:mm:ss' for example){Environment.NewLine}");
+            DateTime point = DateTime.Parse(Console.ReadLine());
+            DirectoryInfo sourceDirectory = new DirectoryInfo(SourcePath);
+            if (sourceDirectory.Exists)
+            {
+                Directory.Delete(SourcePath, true);
+            }
+
+            foreach (string path in Directory.GetDirectories(BackupPath, "*.txt", SearchOption.AllDirectories))
+            {
+                string[] versions = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+                for (int i = versions.Length - 1; i >= 0; i--)
+                {
+                    if (versions[i].Substring(versions[i].LastIndexOf('-')) == @"-deleted")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        DateTime ver = DateTime.ParseExact(versions[i].Substring(versions[i].LastIndexOf('\\') + 1), dateFormat, null);
+                        if (ver <= point)
+                        {
+                            string filePath = versions[i].Remove(versions[i].LastIndexOf('\\')).Replace(BackupPath, SourcePath);
+                            string folderPath = filePath.Remove(filePath.LastIndexOf('\\'));
+                            Directory.CreateDirectory(folderPath);
+                            File.Copy(versions[i], filePath, true);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Backup successful");
+            return;
+        }
+
+        public static void Watch()
+        {
+            Console.WriteLine($"Watching started{Environment.NewLine}");
+            DirectoryInfo sourceDirectory = new DirectoryInfo(SourcePath);
+            if (!sourceDirectory.Exists)
+            {
+                Directory.CreateDirectory(SourcePath);
+            }
+
+            foreach (string filePath in Directory.GetFiles(SourcePath, "*.txt", SearchOption.AllDirectories))
+            {
+                FileInfo fi = new FileInfo(filePath);
+                Directory.CreateDirectory(filePath.Replace(SourcePath, BackupPath));
+                File.Copy(filePath, filePath.Insert(filePath.Length, $@"\{fi.LastWriteTime.ToString(dateFormat)}").Replace(SourcePath, BackupPath), true);
+            }
+
+            FileSystemWatcher watcher = new FileSystemWatcher(SourcePath, "*.txt");
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            watcher.InternalBufferSize = 10485760;
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Renamed += new RenamedEventHandler(OnRename);
+            watcher.Deleted += new FileSystemEventHandler(OnDelete);
+
+            Console.WriteLine($"Press <q> to stop watching{Environment.NewLine}");
+            while (Console.Read() != 'q')
+            {
+            }
+
+            Console.WriteLine($"Watching stopped{Environment.NewLine}");
+            Console.ReadLine();
+        }
+
+        private static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
+
+            return false;
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo(e.FullPath);
+            while (IsFileLocked(fileInfo))
+            {
+                Thread.Sleep(1);
+            }
+
+            File.Copy(e.FullPath, e.FullPath.Insert(e.FullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}").Replace(SourcePath, BackupPath), true);
+        }
+
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            Directory.CreateDirectory(e.FullPath.Replace(SourcePath, BackupPath));
+            OnChanged(source, e);
+        }
+
+        private static void OnDelete(object source, FileSystemEventArgs e)
+        {
+            File.Create(e.FullPath.Insert(e.FullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}-deleted").Replace(SourcePath, BackupPath));
+        }
+
+        private static void OnRename(object source, RenamedEventArgs e)
+        {
+            OnCreated(source, e);
+            File.Create(e.OldFullPath.Insert(e.OldFullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}-deleted").Replace(SourcePath, BackupPath));
+        }
+    }
+}
--- EPAM.Task5/EPAM.Task5.01-BackupSystem/Program.cs
+++ EPAM.Task5/EPAM.Task5.01-BackupSystem/Program.cs
@@ -1,34 +1,24 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace EPAM.Task5._01_BackupSystem
 {
     public class Program
     {
-        private static string sourcePath;
-        private static string backupPath;
-        private static string dateFormat = "yyyy-MM-dd HH-mm-ss";
-
         public static void Main()
         {
             Console.WriteLine($"{Environment.NewLine}Very Simple Backup System{Environment.NewLine}");
             Console.WriteLine("Enter folder path for watch or backup");
             try
             {
-                sourcePath = Console.ReadLine();
-                if (sourcePath[sourcePath.Length - 1] == '\\')
+                BackupSystem.SourcePath = Console.ReadLine();
+                if (BackupSystem.SourcePath[BackupSystem.SourcePath.Length - 1] == '\\')
                 {
-                    sourcePath = sourcePath.Remove(sourcePath.Length - 1);
+                    BackupSystem.SourcePath = BackupSystem.SourcePath.Remove(BackupSystem.SourcePath.Length - 1);
                 }
 
-                backupPath = sourcePath.Insert(sourcePath.Length, "Backup");
+                BackupSystem.BackupPath = BackupSystem.SourcePath.Insert(BackupSystem.SourcePath.Length, "Backup");
             }
-            catch (IndexOutOfRangeException exc)
+            catch (IndexOutOfRangeException)
             {
                 Console.WriteLine("Incorrect folder path");
                 return;
@@ -46,14 +36,14 @@ namespace EPAM.Task5._01_BackupSystem
             {
                 case "1":
                     {
-                        Watch();
+                        BackupSystem.Watch();
                         Main();
                         break;
                     }
 
                 case "2":
                     {
-                        Recovery();
+                        BackupSystem.Recovery();
                         Main();
                         break;
                     }
@@ -63,131 +53,6 @@ namespace EPAM.Task5._01_BackupSystem
                         break;
                     }
             }
-        }
-
-        private static void Watch()
-        {
-            Console.WriteLine($"Watching started{Environment.NewLine}");
-            DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
-            if (!sourceDirectory.Exists)
-            {
-                Directory.CreateDirectory(sourcePath);
-            }
-
-            foreach (string filePath in Directory.GetFiles(sourcePath, "*.txt", SearchOption.AllDirectories))
-            {
-                FileInfo fi = new FileInfo(filePath);
-                Directory.CreateDirectory(filePath.Replace(sourcePath, backupPath));
-                File.Copy(filePath, filePath.Insert(filePath.Length, $@"\{fi.LastWriteTime.ToString(dateFormat)}").Replace(sourcePath, backupPath), true);
-            }
-
-            FileSystemWatcher watcher = new FileSystemWatcher(sourcePath, "*.txt");
-            watcher.IncludeSubdirectories = true;
-            watcher.EnableRaisingEvents = true;
-            watcher.InternalBufferSize = 10485760;
-
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-            watcher.Changed += new FileSystemEventHandler(OnChanged);
-            watcher.Created += new FileSystemEventHandler(OnCreated);
-            watcher.Renamed += new RenamedEventHandler(OnRename);
-            watcher.Deleted += new FileSystemEventHandler(OnDelete);
-
-            Console.WriteLine($"Press <q> to stop watching{Environment.NewLine}");
-            while (Console.Read() != 'q')
-            {
-            }
-
-            Console.WriteLine($"Watching stopped{Environment.NewLine}");
-            Console.ReadLine();
-        }
-
-        private static void OnChanged(object source, FileSystemEventArgs e)
-        {
-            FileInfo fileInfo = new FileInfo(e.FullPath);
-            while (IsFileLocked(fileInfo))
-            {
-                Thread.Sleep(1);
-            }
-
-            File.Copy(e.FullPath, e.FullPath.Insert(e.FullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}").Replace(sourcePath, backupPath), true);
-        }
-
-        private static void OnCreated(object source, FileSystemEventArgs e)
-        {
-            Directory.CreateDirectory(e.FullPath.Replace(sourcePath, backupPath));
-            OnChanged(source, e);
-        }
-
-        private static void OnRename(object source, RenamedEventArgs e)
-        {
-            OnCreated(source, e);
-            File.Create(e.OldFullPath.Insert(e.OldFullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}-deleted").Replace(sourcePath, backupPath));
-        }
-
-        private static void OnDelete(object source, FileSystemEventArgs e)
-        {
-            File.Create(e.FullPath.Insert(e.FullPath.Length, $@"\{DateTime.Now.ToString(dateFormat)}-deleted").Replace(sourcePath, backupPath));
-        }
-
-        private static bool IsFileLocked(FileInfo file)
-        {
-            FileStream stream = null;
-
-            try
-            {
-                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
-            }
-            catch (IOException)
-            {
-                return true;
-            }
-            finally
-            {
-                if (stream != null)
-                {
-                    stream.Close();
-                }
-            }
-
-            return false;
-        }
-
-        private static void Recovery()
-        {
-            Console.WriteLine($"Enter date and time of backup ('dd-MM-yyyy hh:mm:ss' for example){Environment.NewLine}");
-            DateTime point = DateTime.Parse(Console.ReadLine());
-            DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
-            if (sourceDirectory.Exists)
-            {
-                Directory.Delete(sourcePath, true);
-            }
-
-            foreach (string path in Directory.GetDirectories(backupPath, "*.txt", SearchOption.AllDirectories))
-            {
-                string[] versions = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
-                for (int i = versions.Length - 1; i >= 0; i--)
-                {
-                    if (versions[i].Substring(versions[i].LastIndexOf('-')) == @"-deleted")
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        DateTime ver = DateTime.ParseExact(versions[i].Substring(versions[i].LastIndexOf('\\') + 1), dateFormat, null);
-                        if (ver <= point)
-                        {
-                            string filePath = versions[i].Remove(versions[i].LastIndexOf('\\')).Replace(backupPath, sourcePath);
-                            string folderPath = filePath.Remove(filePath.LastIndexOf('\\'));
-                            Directory.CreateDirectory(folderPath);
-                            File.Copy(versions[i], filePath, true);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine($"Backup successful");
-            return;
         }
     }
 }
