6fb12356580bfc02c34390a0c33a991b440b7105
Alexander <Alexander@DESKTOP-89VB63U>
2019-04-18 17:49:47 +0400

    Rename

---
+++ EPAM.Final/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var postLogic = EPAM.Final_Common.DependencyResolver.PostLogic;
+            var threadLogic = EPAM.Final_Common.DependencyResolver.ThreadLogic;
+            Console.WriteLine();
+            Console.WriteLine(threadLogic.GetId("tested"));
+            Console.WriteLine(postLogic.New("hello", 40, "admin"));
+        }
+    }
+}
